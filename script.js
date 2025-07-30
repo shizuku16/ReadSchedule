22713ca8fb1d3d7d722b0e13265f91f198d0a3cf
shizuku16 <89346844+shizuku16@users.noreply.github.com>
2025-07-31 03:43:52 +0900

    Add files via upload
    ・読み込み中の待機画面の表示
    ・クエリの追加（表示月・メンバー）

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     const thisMonth=new Date().getMonth()+1;
     const select=document.getElementById("month");
     let string="";
-    let selectMonth=thisMonth+1
+    let selectMonth=new Date().getDate()>=15?thisMonth+1:thisMonth
     for(let i=1;i<=selectMonth;i++) string+=`<option>${i}月</option>`;
     select.insertAdjacentHTML("beforeend",string);
     select.value=`${thisMonth}月`;
 
+    //年月のクエリがある場合、その情報で表示する
+    const url=new URL(window.location.href);
+    if(url.searchParams.has("year") && url.searchParams.has("month")){
+        const year = url.searchParams.get("year");
+        const month = url.searchParams.get("month");
+        //読み込みなおすために一度該当月のキャッシュを消す
+        window.sessionStorage.removeItem((`${year}${month}`));
+
+        //再度設定し読み込みなおす
+        document.getElementById("year").value=`${year}年`;
+        select.value=`${month}月`;  
+        await dataGet();
+        
+        //メンバーのクエリがある場合、その情報で表示する
+        memberDisplay(url);
+    }
 });
 
-function dataGet(){
+//メンバーのクエリがある場合、その情報で表示する関数
+function memberDisplay(url){
+    if(url.searchParams.has("member")){
+        const allMember = url.searchParams.getAll("member");
+        url.searchParams.delete("member");
+        window.history.replaceState({}, '', url);
+        allMember.forEach(value=>{
+            document.getElementById("member").value=value;
+            display();
+        })
+    }
+}
+
+//表示月が変わったときの処理
+async function dataGet(){
     const year=Number(document.getElementById("year").value.replace("年",""));
     const month=Number(document.getElementById("month").value.replace("月",""));
     const member=document.getElementById("member");
+    //クエリに表示する年月を追加
+    const url = new URL(window.location.href);
+    url.searchParams.set("year",year);
+    url.searchParams.set("month",month);
+    // アドレスバーを更新
+    window.history.replaceState({}, '', url);
+
     while(member.firstChild ){
         member.removeChild( member.firstChild );
     }
@@ -23,12 +60,14 @@ function dataGet(){
             string+=`<option>${Object.keys(data)[i]}</option>`;
         member.insertAdjacentHTML("beforeend",string);
         member.value="";
-        makeDateTable();
+        await makeDateTable();
+        memberDisplay(url);
         return;
     }
     member.disalbed=true;
 
-    fetch(`https://script.google.com/macros/s/AKfycbwIFhLsMlqMGEhSzNBJJDscjV3P-lAh9n_KY0_4XY36oZu7gl0difbRNpggBRY3dNxa/exec?data=${year},${month}`)
+    showLoading();
+    await fetch(`https://script.google.com/macros/s/AKfycbwIFhLsMlqMGEhSzNBJJDscjV3P-lAh9n_KY0_4XY36oZu7gl0difbRNpggBRY3dNxa/exec?data=${year},${month}`)
     .then(res=>res.json())
     .then(data=>{
         window.sessionStorage.setItem(`${year}${month}`,JSON.stringify(data));
@@ -40,8 +79,14 @@ function dataGet(){
         makeDateTable();
         member.disalbed=false;
     })
+    .then(function(){
+        console.log(url)
+        memberDisplay(url);
+        hideLoading();        
+    })
 }
 
+//日付部分のTable作成
 function makeDateTable(){
     const year=Number(document.getElementById("year").value.replace("年",""));
     const month=Number(document.getElementById("month").value.replace("月",""));
@@ -76,9 +121,11 @@ function makeDateTable(){
     for(let i=1;i<=lastDay;i++)
         string+=`<tr bgcolor="${data.color[i+1]}"><td id="${year}y${month}m${i}d">${month}/${i}(${day[new Date(year,month-1,i).getDay()]})</td></tr>`;
     string+=`<tr id="bikou"><td>備考</td></tr>`;
-    table.insertAdjacentHTML("beforeend",string)
+    table.insertAdjacentHTML("beforeend",string);
+    return
 }
 
+//メンバーを増やしたときの処理
 function display(){
     const year=Number(document.getElementById("year").value.replace("年",""));
     const month=Number(document.getElementById("month").value.replace("月",""));
@@ -100,9 +147,16 @@ function display(){
         table.removeChild( table.firstChild );
     }
     table.insertAdjacentHTML("beforeend",tableData.join(`</tr>`))
-    document.getElementById("table").style.width="2ch"
+    document.getElementById("table").style.width="2ch";
+
+    //クエリに情報追加
+    const url = new URL(window.location.href);
+    url.searchParams.append("member",member);
+    // アドレスバーを更新
+    window.history.replaceState({}, '', url);
 }
 
+//メンバーを減らしたときの処理
 function deleteColumn(e){
     const member=e.id.replace("delete","")
     document.getElementById(member).remove();
@@ -111,4 +165,11 @@ function deleteColumn(e){
         document.getElementById(member+i).remove();
     }
     document.getElementById(member+"memo").remove();
+    //クエリを一度取得
+    const url = new URL(window.location.href);
+    const values=url.searchParams.getAll("member").filter(e=>e!=member);
+    url.searchParams.delete("member");
+    values.forEach(e=>url.searchParams.append("member",e));
+    // アドレスバーを更新
+    window.history.replaceState({}, '', url);
 }
